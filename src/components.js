112fc3e5209401914dcc4b91f70f147a2f22bd05
sh1okoh <kooooo.0719task@gmail.com>
2019-08-14 15:31:42 +0900

    課題1ができました

--- src/components.js
+++ src/components.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { calculateWinner } from "./utils.js";
 
 function Square(props) {
   const classNameValue = `square ${props.isHighlight ? 'highlight' : ''}`
@@ -100,27 +100,4 @@ export class Game extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return {
-        squares: squares[a],
-        line: lines[i]
-      };
-    }
-  }
-  return null;
 }
